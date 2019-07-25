8808880d9e628d20b8ca7c60b6184288c86bac9c
miguelpc94 <miguelpdecastro@gmail.com>
2019-07-24 21:55:01 -0300

    TweetReader implemented

--- cpp/tweet_reader.cpp
+++ cpp/tweet_reader.cpp
@@ -1,7 +1,11 @@
+// Make sure the module (import "env" "memory" (memory $0 1)) 
+// is present on the wasm to import memory
+
 unsigned char* compute(unsigned int inputSize, unsigned int* outputSize, unsigned char* input) {
   const char* command = "Say something";
+  const unsigned char commandSize = 13;
   bool commandValid = true;
-  for (unsigned int i=0; i<inputSize; i++) {
+  for (unsigned int i=0; i<commandSize; i++) {
     if (input[i]!=command[i]) {
       commandValid = false;
     }
